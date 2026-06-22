f58bef0320d6b00f7fddee505e7bf46f5a0debfa
Dima <nik-dima07@mail.ru>
2026-06-22 23:14:00 +0300

    ADD: утилита consLog для логирования

---
+++ src/utils/consLog.js
@@ -0,0 +1,10 @@
+function consLog(prompt, ...args){
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    console.log(`[ ${hours}:${minutes}:${seconds} ] ${prompt}`, ...args);
+}
+
+module.exports = { consLog };
