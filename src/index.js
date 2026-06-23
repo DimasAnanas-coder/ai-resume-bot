7883d6db058e9a37e6feca53d1049b12eee2e71a
Dima <nik-dima07@mail.ru>
2026-06-23 21:08:08 +0300

    ADD: jsconfig

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const { createBot } = require("./bot/bot.js");
 const { consLog } = require("./utils/consLog.js");
 
-bot = createBot();
-consLog("Бот успешно собран");
+function main(){
+    bot = createBot();
+    consLog("Бот успешно собран");
 
-bot.launch()
-consLog("Бот успешно запущен");
+    bot.launch()
+    consLog("Бот успешно запущен");
+}
+
+main();
