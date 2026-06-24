1ba728e0793b5695dddfb3aa74d95daa6c7fa6d0
Dima <nik-dima07@mail.ru>
2026-06-24 19:02:36 +0300

    ADD: новые кнопки главного меню, универсальный обработчик редактирования сообщения и отправки нового для главного меню. Добавлены await вызовы

--- src/bot/handlers/menu.js
+++ src/bot/handlers/menu.js
@@ -1,13 +1,36 @@
 const { Context } = require("telegraf");
 const { bot } = require("../bot.js");
-const { MENU_CALLBACK } = require("../../config/constants")
+const { MENU_CALLBACK, BACK_TO_MENU_CALLBACK } = require("../../config/constants")
+const { mainMenuKeyboard } = require("../keyboards/mainMenu.js")
+
+/**
+* @param {Context} ctx
+* @param {CallableFunction} sendFunc
+*/
+async function menuHandle(ctx, sendFunc){
+    const resumeAvailableCount = 1;
+    await sendFunc.call(
+        ctx, 
+        `Количество доступных резюме: ${resumeAvailableCount}
+Вы можете пополнить баланс по кнопке ниже`,
+        { reply_markup: mainMenuKeyboard }
+    );
+    await ctx.answerCbQuery();
+}
 
 /**
 * @param {Context} ctx
 */
-async function menuHandle(ctx){
-    ctx.reply(`Количество доступных резюме: ${1}
-Вы можете пополнить баланс по кнопке ниже`);
-    ctx.answerCbQuery();
+async function entryToMenuHandle(ctx){
+    await menuHandle(ctx, ctx.reply);
 }
-bot.action(MENU_CALLBACK, menuHandle);
+bot.action(MENU_CALLBACK, entryToMenuHandle);
+
+
+/**
+* @param {Context} ctx
+*/
+async function backToMenuHandle(ctx){
+    await menuHandle(ctx, ctx.editMessageText);
+}
+bot.action(BACK_TO_MENU_CALLBACK, backToMenuHandle);
--- src/config/constants/callbackData.js
+++ src/config/constants/callbackData.js
@@ -1,3 +1,7 @@
 module.exports = {
     MENU_CALLBACK: "entryToMenu",
+    BACK_TO_MENU_CALLBACK: "backToMenu",
+    GENERATE_RESUME_CALLBACK: "generateResume",
+    PAYMENT_CALLBACK: "entryToPayment",
+    BACK_RO_PAYMENT_CALLBACK: "backToPayment"
 }
